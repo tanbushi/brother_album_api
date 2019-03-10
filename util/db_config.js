658b37f564a782440bdafa693fbf493fa1b2a38b
pandaomeng <806636588@qq.com>
2019-03-10 15:21:38 +0800

    删除数据库配置文件

--- util/db_config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mysql: {
-    host: 'xxx.com', // 改为自己的 host
-    user: 'root', // 改为自己的 user
-    password: 'xxxxxx', // 改为自己的 password
-    database: 'brother_album',
-  },
-}
