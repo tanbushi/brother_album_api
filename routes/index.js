acc1af4c5cf7a748910de19e1759900b7473a0e4
pandaomeng <806636588@qq.com>
2019-03-10 15:23:34 +0800

    index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 const router = require('koa-router')()
+const { query } = require('../util/db')
+
+async function selectAllUser() {
+  const sql = 'select * from user'
+  const dataList = await query(sql)
+  return dataList
+}
 
 router.get('/', async (ctx) => {
+  const dataList = await selectAllUser()
+  console.log(dataList)
+  // return dataList
   await ctx.render('index', {
     title: 'Hello Koa 2!',
   })
